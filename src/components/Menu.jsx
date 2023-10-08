24a9e98b17284af897d69a2434aeb30ddfd077d7
Huascar Gutierrez <huascargutierrezcastro9951591@gmail.com>
2023-10-07 23:44:43 -0400

    with css and styles

---
+++ src/components/Menu.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../styles/menu.css'
+function Menu() {
+  return (
+    <footer>
+        <nav>
+            <ul>
+				<li><img src='/menu/Home.svg'></img></li>
+				<li><img src='/menu/Favorite.svg'></img></li>
+				<li><img src='/menu/Chats.svg'></img></li>
+				<li><img src='/menu/Settings.svg'></img></li>
+			</ul>
+        </nav>
+    </footer>
+  )
+}
+
+export default Menu
