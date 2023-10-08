94d748b473984fbcd5d3ed782cd414d992ac7b6e
Helen Catacora <catacorapatanahelenjazmin@gmail.com>
2023-10-07 21:26:34 -0400

    Creando archivos

---
+++ src/components/carrusel.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function carrusel() {
+  return (
+    <section>
+        <div>
+            <div>
+                <i></i>
+                <span></span>
+            </div>
+            <button></button>
+        </div>
+    </section>
+  )
+}
+
+export default carrusel
