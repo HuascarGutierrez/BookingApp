69e3ab5c4f6e03fde9ffacc242d71dfaedb7d123
Huascar Gutierrez <huascargutierrezcastro9951591@gmail.com>
2023-10-07 23:29:08 -0400

    implementing html

---
+++ src/components/Menu.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Menu() {
+  return (
+            <ul>
+				<li><img src='/menu/Home.svg'></img></li>
+				<li><img src='/menu/Favorite.svg'></img></li>
+				<li><img src='/menu/Chats.svg'></img></li>
+				<li><img src='/menu/Settings.svg'></img></li>
+			</ul>
+  )
+}
+
+export default Menu
