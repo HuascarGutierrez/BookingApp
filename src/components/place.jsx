fb69d13db5aaed0ea559f8cd034f0a14e3312024
¨Elian2804¨ <bolchique11@gmail.com>
2023-10-10 08:24:06 -0400

    list

---
+++ src/components/place.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../styles/place"
+
+function place() {
+  return (
+    <section>
+        <div className='place'>
+            <button className='list'></button>
+            <img src="" alt="" />
+        </div>
+    </section>
+  )
+}
+
+export default place
