7f7becbdc4d936911c5c70b97ae3acee07dccdaf
Huascar Gutierrez <huascargutierrezcastro9951591@gmail.com>
2023-10-10 20:05:56 -0400

    primer debug con el carrusel

--- src/assets/places.js
+++ src/assets/places.js
@@ -6,6 +6,22 @@ export const places = [
         size:"2 ",
         price:"920",
         score:"4,93",
-        imgUrl:"/place1.png"
+        imgUrl:"/place1.svg"
+    },
+    {
+        title:"Villa, asdfasdf",
+        bedroom: "22 ",
+        size:"223 ",
+        price:"123",
+        score:"1,56",
+        imgUrl:"/place2.svg"
+    },
+    {
+        title:"Villa, asdfasdf",
+        bedroom: "22 ",
+        size:"223 ",
+        price:"123",
+        score:"1,56",
+        imgUrl:"/place2.svg"
     }
 ]
