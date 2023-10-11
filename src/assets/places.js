e5d2567989d9a247abc33a6cb6a30af09cba018f
Helen Catacora <catacorapatanahelenjazmin@gmail.com>
2023-10-10 22:46:13 -0400

    prueba

--- src/assets/places.js
+++ src/assets/places.js
@@ -3,9 +3,9 @@ export const places = [
     {
         title:"Villa, Kemah Tinggi",
         bedroom: "2 ",
-        size:"2 ",
+        size:"214 ",
         price:"920",
         score:"4,93",
-        imgUrl:"/place1.png"
+        imgUrl:"/place1.svg"
     }
 ]
