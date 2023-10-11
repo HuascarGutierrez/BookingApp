dfe44b030e01af5e7fe645021ec72a6c96b44f90
DenisHV <denishuanca77292121@gmail.com>
2023-10-11 10:40:25 -0300

    moleculas

--- src/components/Boton.jsx
+++ src/components/Boton.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import "../styles/input.css"
 const icon = '/Group3.svg'
 
-function Input() {
+function Boton() {
   return (
     <div>
         <div class="DENIS">
-            <input type="text" placeholder="Enter city or region"></input>
+            <button>
+                <img src='/Group3.svg'></img>
+            </button>
         </div>
     </div>
   )
 }
-export default Input
+
+export default Boton
