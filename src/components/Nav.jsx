e48dc64a5d14f8f4d9116be199a498fb98a96820
¨[Serghio07]¨ <[ticona465@gmail.com]>
2023-10-10 22:25:32 -0400

    listo

---
+++ src/components/Nav.jsx
@@ -0,0 +1,17 @@
+
+
+function Nav() {
+  return (
+     <nav>
+     <ul>
+        <li><a href="bets">Bests</a></li>
+        <li><a href="popular">Popular</a></li>
+        <li><a href="immediate">Immediate</a></li>
+        <li><a href="new">New</a></li>
+        <li><a href="profitable">Profitable</a></li>
+     </ul>
+     </nav>
+  )
+}
+
+export default Nav
