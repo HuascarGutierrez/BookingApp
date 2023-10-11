5dececa60ab185e8f6016fb5d417ac67434e92e7
DenisHV <denishuanca77292121@gmail.com>
2023-10-11 10:05:37 -0300

    Terminado

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "../styles/input.css"
+const icon = '/Group3.svg'
 
 function Input() {
   return (
@@ -7,13 +8,8 @@ function Input() {
         <div class="DENIS">
             <input type="text" placeholder="Enter city or region"></input>
             <button>
-                <i><svg width="26" height="17" viewBox="0 0 26 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <rect y="3" width="26" height="2" rx="1" fill="white"/>
-                    <rect y="12" width="26" height="2" rx="1" fill="white"/>
-                    <circle cx="9" cy="4" r="3" fill="#61AAC9" stroke="white" stroke-width="2"/>
-                    <circle cx="18" cy="13" r="3" fill="#61AAC9" stroke="white" stroke-width="2"/>
-                    </svg>
-                    </i>
+                <img src='/Group3.svg'></img>
+
             </button>
         </div>
     </div>
