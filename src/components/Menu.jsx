1874a78768f62ef95715eeb53c1dcba36b4be80d
Huascar Gutierrez <huascargutierrezcastro9951591@gmail.com>
2023-10-10 20:32:00 -0400

    ???

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -8,6 +8,7 @@ function Menu() {
 				<li><img src='/menu/Home.svg'></img></li>
 				<li><img src='/menu/Favorite.svg'></img></li>
 				<li><img src='/menu/Chats.svg'></img></li>
+				<li><img src='/menu/Settings.svg'></img></li>
 			</ul>
         </nav>
     </div>
