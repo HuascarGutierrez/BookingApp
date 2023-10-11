ac696c37dfe6e6c0131703c17ffd6a04a51169d8
Huascar Gutierrez <huascargutierrezcastro9951591@gmail.com>
2023-10-10 20:31:22 -0400

    solving date

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -8,7 +8,6 @@ function Menu() {
 				<li><img src='/menu/Home.svg'></img></li>
 				<li><img src='/menu/Favorite.svg'></img></li>
 				<li><img src='/menu/Chats.svg'></img></li>
-				<li><img src='/menu/Settings.svg'></img></li>
 			</ul>
         </nav>
     </div>
