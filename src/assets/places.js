943c3188b4d0ff6f2405c9957753dc212d80a94a
Huascar Gutierrez <huascargutierrezcastro9951591@gmail.com>
2023-10-12 06:41:53 -0400

    with places

--- src/assets/places.js
+++ src/assets/places.js
@@ -9,19 +9,35 @@ export const places = [
         imgUrl:"/place1.svg"
     },
     {
-        title:"Villa, Kemah Tinggi",
-        bedroom: "2 ",
-        size:"214 ",
-        price:"920",
-        score:"4,93",
-        imgUrl:"/place1.svg"
+        title:"Villa, Ziba Seminyak",
+        bedroom: "5 ",
+        size:"520 ",
+        price:"1100",
+        score:"4,89",
+        imgUrl:"/place2.svg"
     },
     {
-        title:"Villa, Kemah Tinggi",
+        title:"Villa, Kecamatan Ubud",
         bedroom: "2 ",
         size:"214 ",
         price:"920",
-        score:"4,93",
-        imgUrl:"/place1.svg"
-    }
+        score:"4,89",
+        imgUrl:"/place3.svg"
+    },
+{
+    title:"Villa, Canggu",
+    bedroom: "5 ",
+    size:"600 ",
+    price:"400",
+    score:"4,82",
+    imgUrl:"/place4.svg"
+    },
+{
+    title:"Villa, Kerobocan Kelod",
+    bedroom: "4 ",
+    size:"1000 ",
+    price:"750",
+    score:"4,80",
+    imgUrl:"/place5.svg"
+    }
 ]
