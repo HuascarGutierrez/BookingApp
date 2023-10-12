a767aebecebde7fb3c7a94b4d8b172147b341a62
Huascar Gutierrez <huascargutierrezcastro9951591@gmail.com>
2023-10-12 03:16:36 -0400

    solving country

---
+++ src/components/Place.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "../styles/place.css"
+
+function Place() {
+  return (
+    <section>
+        <p>You are here</p>
+        <div className='country'>
+        <h2>Indonesia</h2>
+        <div className='arrowPlace'>
+        <i className='arrow'></i>
+        </div>
+        </div>
+    </section>
+  )
+}
+
+export default Place
