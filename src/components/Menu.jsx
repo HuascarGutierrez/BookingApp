9ffccc2f0497cee82bee8e872f20c00138c237be
Huascar Gutierrez <huascargutierrezcastro9951591@gmail.com>
2023-10-19 00:03:58 -1200

    last

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,10 +4,10 @@ function Menu() {
     <div>
         <nav className='navBottom'>
             <ul>
-				<li><img src='/menu/Home.svg'></img></li>
-				<li><img src='/menu/Favorite.svg'></img></li>
-				<li><img src='/menu/Chats.svg'></img></li>
-				<li><img src='/menu/Settings.svg'></img></li>
+				<li><img src='./menu/Home.svg'></img></li>
+				<li><img src='./menu/Favorite.svg'></img></li>
+				<li><img src='./menu/Chats.svg'></img></li>
+				<li><img src='./menu/Settings.svg'></img></li>
 			</ul>
         </nav>
     </div>
